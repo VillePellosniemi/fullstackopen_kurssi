00b82dc467e6fd515443a294e0f95b06880402ad
VillePellosniemi <34025719+VillePellosniemi@users.noreply.github.com>
2019-06-12 14:10:35 +0300

    update

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,6 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  console.log(props)
+  return (
+      <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+      <div>
+        <p>{props.part} {props.exercises}</p>
+      </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+      <p>Total amount of exercises: {props.total}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -12,17 +34,11 @@ const App = () => {
 
   return (
       <div>
-        <h1>{course}</h1>
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+        <Header course={course} />
+        <Content part={part1} exercises={exercises1}/>
+        <Content part={part2} exercises={exercises2}/>
+        <Content part={part3} exercises={exercises3}/>
+        <Total total={exercises1+exercises2+exercises3} />
       </div>
   )
 }
